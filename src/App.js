28b1adf4864cda350db27cc1d1b16234556e04fa
Luke Gronert <lukegronert@gmail.com>
2022-07-18 00:25:34 -0700

    Set up react-router-dom and Provider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Header from './components/Header';
+import UserListing from './components/UserListing';
+import UserDetail from './components/UserDetail';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<UserListing />} />
+          <Route path="users/:userLogin" exact element={<UserDetail />} />
+          <Route path="*" element={<h1>404 Page Not Found</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
